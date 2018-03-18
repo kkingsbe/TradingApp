ba1acda1c35dd68b76dfbe2f6bfc27e0b1f10e53
Kyle <kkingsbe@gmail.com>
2018-03-18 16:58:48 -0400

    Added binance library

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -9,6 +9,10 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main extends Application {
 
     @Override
@@ -17,10 +21,23 @@ public class Main extends Application {
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
+
+        getKeys();
     }
 
-
+    public List getKeys(){
+        Scanner key = new Scanner(System.in);
+        System.out.println("API KEY:    ");
+        String BINANCE_API_KEY = key.next();
+        System.out.println("BINANCE_SECRET_KEY:    ");
+        String BINANCE_SECRET_KEY = key.next();
+        List keys = new ArrayList();
+        keys.add(BINANCE_API_KEY);
+        keys.add(BINANCE_SECRET_KEY);
+        return keys;
+    }
     public static void main(String[] args) {
         launch(args);
     }
+
 }
