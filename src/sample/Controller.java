99d62a160d3b34c9376c1c6b796952434e7760c7
Kyle <kkingsbe@gmail.com>
2018-03-18 17:25:30 -0400

    Fixed keys looping twice

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,8 +1,12 @@
 package sample;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Controller {
     Main run = new Main();
-    final String BINANCE_API_KEY = run.getKeys().get(0).toString();
-    final String BINANCE_SECRET_KEY = run.getKeys().get(1).toString();
+    List keysList = new ArrayList(run.getKeys());
+    final String BINANCE_API_KEY = keysList.get(0).toString();
+    final String BINANCE_SECRET_KEY = keysList.get(1).toString();
 
 }
