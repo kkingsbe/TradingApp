0a4d857fadbf66761859b9cba35410267a72fcb5
Kyle <kkingsbe@gmail.com>
2018-03-19 19:22:56 -0400

    Added support for more altcoins

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -17,7 +17,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("CryptoClient");
         primaryStage.setScene(new Scene(root, 878, 543));
         primaryStage.show();
 
